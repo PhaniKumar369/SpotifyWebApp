59df591ffd591668256f9d829b09ca46275db01b
PhaniKumar369 <130128165+PhaniKumar369@users.noreply.github.com>
2023-04-07 11:21:47 +0530

    Add files via upload

---
+++ spotifyApp/script.js
@@ -0,0 +1,121 @@
+console.log("Welcome to Spotify")
+
+let songIndex=0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay =document.getElementById('masterPlay');
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+
+let songs=[
+    { songName:"Kesariya", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    { songName:"ChannaMereya ", filePath:"songs/2.mp3", coverPath:"covers/2.jpg"},
+    { songName:"Pal", filePath:"songs/3.mp3", coverPath:"covers/3.jpg"},
+    { songName:"Vennelave", filePath:"songs/4.mp3", coverPath:"covers/4.jpg"},
+    { songName:"MateRani", filePath:"songs/5.mp3", coverPath:"covers/5.jpg"},
+    { songName:"Avunanavaa", filePath:"songs/6.mp3", coverPath:"covers/6.jpg"},
+    { songName:"KanunnaKalyanam", filePath:"songs/7.mp3", coverPath:"covers/7.jpg"},
+    { songName:"Sanchari", filePath:"songs/8.mp3", coverPath:"covers/8.jpg"},
+    { songName:"Amma", filePath:"songs/9.mp3", coverPath:"covers/9.jpg"},
+    { songName:"OhSita", filePath:"songs/10.mp3", coverPath:"covers/10.jpg"}
+]
+songItems.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+
+})
+
+//Handle the play/pause click 
+masterPlay.addEventListener('click',()=>{
+    if (audioElement.paused|| audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+//Listen to Events
+audioElement.addEventListener('timeupdate',()=>{
+
+    //update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    // console.log(progress);
+    myProgressBar.value = progress; 
+}) 
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.add('fa-play-circle');
+        element.classList.remove('fa-pause-circle');
+
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+
+        
+    })
+})
+
+document.getElementById('next').addEventListener('click',()=>{
+    if (songIndex>=9){
+        songIndex = 0;
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+   
+})
+
+document.getElementById('previous').addEventListener('click',()=>{
+    if (songIndex<=0){
+        songIndex = 0;
+    }
+    else{
+        songIndex-=1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+   
+})
+
+
+
+
+
+
